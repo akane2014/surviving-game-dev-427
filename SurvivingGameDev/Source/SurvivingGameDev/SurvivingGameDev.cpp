d623978fbf01abe20381de6fc21d7e46bba06c9d
Ming Zhang <akane2002@gmail.com>
2024-06-16 00:45:57 +1200

    First commit

---
+++ SurvivingGameDev/Source/SurvivingGameDev/SurvivingGameDev.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SurvivingGameDev.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivingGameDev, "SurvivingGameDev" );
---
+++ SurvivingGameDev/Source/SurvivingGameDev/SurvivingGameDevGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurvivingGameDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVINGGAMEDEV_API ASurvivingGameDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
